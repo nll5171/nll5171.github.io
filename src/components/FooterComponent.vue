26a6a065ed98dded6d5625e369a1c8bd5973eb2f
GaryButternubs <GaryButternubs123@gmail.com>
2025-09-06 19:30:59 -0400

    Added styling and created Navbar and Footer component.

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div class="text-section">
+      <div class="container text-light">
+        <div class="row align-items-center">
+          <div class="col pt-3 px-md-5">
+            <h1 id="contact" class="text-center">Contact Me</h1>
+            <p class="text-center">
+              Feel free to reach me at
+              <a href="mailto:nll5171@rit.edu?Subject=Hey!">nll5171@rit.edu</a>. I'd be happy to
+              respond to your email as soon as possible.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+defineProps({
+  options: Array,
+})
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
+    <div class="container">
+      <RouterLink to="/" class="navbar-brand noselect">Nicholas Lang</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      ></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li v-for="option in options" :key="option.title" class="nav-item">
+            <RouterLink :to="`${option.path}`" class="nav-link">{{ option.title }}</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: var(--nav-color);
+}
+</style>
